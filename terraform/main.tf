cfc7a3d70b7f59ff4483cc2a71e1a39dce222a85
Arsen Arutyunyan <aarutyunyan@griddynamics.com>
2016-09-05 19:28:42 +0300

    Add variables file and define public subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,41 @@
 provider "aws" {
-  region  = "us-west-2"
-  profile = "default"   
+  region  = "${var.aws_region}"
+  profile = "default"
 }
 
-resource "aws_instance" "test" {
-  ami = "ami-20be7540"
-  instance_type = "t2.micro"
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
 }
+
+###################################################
+##  Public subnet for ELB, Proxy, Bastion hosts  ##  
+###################################################
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id = "${aws_vpc.default.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.default.id}"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "10.0.0.0/24"
+}
+
+
+###################################################
+##     Private subnet for all another stuff      ##  
+###################################################
+
+#
+# TODO
+#
+
+###################################################
+##                Instances                      ##  
+###################################################
 
